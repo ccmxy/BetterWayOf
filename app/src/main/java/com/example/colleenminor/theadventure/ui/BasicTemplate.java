1b0bd214dd3d4177373caa1d922696beec8dd4c7
ccmxy <windrainicefire@gmail.com>
2015-10-29 12:36:01 -0700

    improve template

--- app/src/main/java/com/example/colleenminor/theadventure/ui/BasicTemplate.java
+++ app/src/main/java/com/example/colleenminor/theadventure/ui/BasicTemplate.java
@@ -1,63 +1,86 @@
 package com.example.colleenminor.theadventure.ui;
 
-import android.content.Context;
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Toast;
+/*
 
-import com.example.colleenminor.theadventure.R;
-import com.example.colleenminor.theadventure.models.Item;
-import com.example.colleenminor.theadventure.models.User;
+public class TwistyActivity extends AppCompatActivity {
+    private User mUser;
+    private SharedPreferences mPreferences;
 
-/**
- * Created by colleenminor on 10/29/15.
- */
-public class BasicTemplate extends AppCompatActivity {
-
-        private User mUser;
-        private SharedPreferences mPreferences;
+    //Common textViews:
+    private TextView mOptionChoice1; //re-light candle
+    private TextView mOptionChoice2; //go upstairs (moaning)
+    private TextView mOptionChoice3; //go downstairs (ocean)
+    //Common textView end
 
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_twisty);
-            setTheItemButton();
-            getPreferencesAndUser();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_twisty);
+        setTheItemButton();
+        getPreferencesAndUser();
+
+        mOptionChoice1 = (TextView) findViewById(R.id.optionChoice1);
+        mOptionChoice2 = (TextView) findViewById(R.id.optionChoice2);
+        mOptionChoice3 = (TextView) findViewById(R.id.optionChoice3);
+
+        mOptionChoice1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
 
 
-        }
+        mOptionChoice2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-        private void getPreferencesAndUser() {
-            mPreferences = getApplicationContext().getSharedPreferences("TheAdventure", Context.MODE_PRIVATE);
-            String username =  mPreferences.getString("username", null);
-            mUser = User.find(username);
-        }
+            }
+        });
 
-        private void setTheItemButton() {
-            FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-            fab.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    Intent intent = new Intent(TwistyActivity.this, ItemsListActivity.class);
-                    startActivity(intent);
-                }
-            });
-        }
 
-        private void addItem(String itemName) {
-            Item item = new Item(itemName, mUser);
-            item.save();
-            Toast.makeText(this, mUser.getName() + ", candlestick has been added to your inventory", Toast.LENGTH_LONG).show();
+        mOptionChoice3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-        }
+            }
+        });
+        //Where other stuff goes
+
+
+
+
+        //End of custom stuffs
 
 
     }
 
+    private void getPreferencesAndUser() {
+        mPreferences = getApplicationContext().getSharedPreferences("TheAdventure", Context.MODE_PRIVATE);
+        String username =  mPreferences.getString("username", null);
+        mUser = User.find(username);
+    }
+
+    //Have to set the correct intent for this one
+    private void setTheItemButton() {
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(TwistyActivity.this, ItemsListActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void addItem(String itemName) {
+        Item item = new Item(itemName, mUser);
+        item.save();
+        Toast.makeText(this, mUser.getName() + ", candlestick has been added to your inventory", Toast.LENGTH_LONG).show();
+
+    }
 
 
+}
+*/
--- app/src/main/java/com/example/colleenminor/theadventure/ui/TwistyActivity.java
+++ app/src/main/java/com/example/colleenminor/theadventure/ui/TwistyActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.colleenminor.theadventure.R;
@@ -17,6 +18,12 @@ public class TwistyActivity extends AppCompatActivity {
     private User mUser;
     private SharedPreferences mPreferences;
 
+    //Common textViews:
+    private TextView mOptionChoice1; //re-light candle
+    private TextView mOptionChoice2; //go upstairs (moaning)
+    private TextView mOptionChoice3; //go downstairs (ocean)
+    //Common textView end
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +31,39 @@ public class TwistyActivity extends AppCompatActivity {
         setContentView(R.layout.activity_twisty);
         setTheItemButton();
         getPreferencesAndUser();
+
+        mOptionChoice1 = (TextView) findViewById(R.id.optionChoice1);
+        mOptionChoice2 = (TextView) findViewById(R.id.optionChoice2);
+        mOptionChoice3 = (TextView) findViewById(R.id.optionChoice3);
+
+        mOptionChoice1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+        mOptionChoice2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+        mOptionChoice3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        //Where other stuff goes
+
+
+
+
+        //End of custom stuffs
 
 
     }
@@ -34,6 +74,7 @@ public class TwistyActivity extends AppCompatActivity {
         mUser = User.find(username);
     }
 
+    //Have to set the correct intent for this one
     private void setTheItemButton() {
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
