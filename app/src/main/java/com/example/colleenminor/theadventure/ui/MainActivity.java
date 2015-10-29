ff19224bd766590245b836a67a721451c165c251
ccmxy <windrainicefire@gmail.com>
2015-10-29 11:55:51 -0700

    Finally got rid of toolbar!

--- app/src/main/java/com/example/colleenminor/theadventure/ui/MainActivity.java
+++ app/src/main/java/com/example/colleenminor/theadventure/ui/MainActivity.java
@@ -6,7 +6,6 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -30,8 +29,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
         mTakeItem = (TextView) findViewById(R.id.takeItem);
         mNextRoom = (TextView) findViewById(R.id.nextLagoon);
         mActionsRemaining = (TextView) findViewById(R.id.actionsRemaining);
