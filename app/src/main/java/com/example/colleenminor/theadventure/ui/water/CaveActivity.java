6ab30ab46c034a12e4c0fa6bea40adc2c7ac5180
ccmxy <windrainicefire@gmail.com>
2015-10-31 14:28:12 -0700

    Eel scene added

--- app/src/main/java/com/example/colleenminor/theadventure/ui/water/CaveActivity.java
+++ app/src/main/java/com/example/colleenminor/theadventure/ui/water/CaveActivity.java
@@ -1,8 +1,10 @@
 package com.example.colleenminor.theadventure.ui.water;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.colleenminor.theadventure.R;
@@ -13,7 +15,9 @@ public class CaveActivity extends AppCompatActivity {
     private TextView mCheckRight;
     private TextView mMoveForward;
     private TextView mNothingHere;
+    private ImageView mWiredEel;
     private int mSegmentTracker;
+    private TextView mScaryEel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +30,9 @@ public class CaveActivity extends AppCompatActivity {
         mCheckRight = (TextView) findViewById(R.id.checkRight);
         mMoveForward = (TextView) findViewById(R.id.moveForward);
         mNothingHere = (TextView) findViewById(R.id.nothingHere);
+        mWiredEel = (ImageView) findViewById(R.id.wiredEel);
+        mScaryEel = (TextView) findViewById(R.id.scaryEel);
+
 
 
 
@@ -51,6 +58,14 @@ public class CaveActivity extends AppCompatActivity {
             }
         });
 
+        mScaryEel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CaveActivity.this, EelActivity.class);
+                startActivity(intent);
+            }
+        });
+
 
         mNothingHere.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -58,7 +73,7 @@ public class CaveActivity extends AppCompatActivity {
 
                 mCaveSegment.setVisibility(View.VISIBLE);
                 mCheckLeft.setVisibility(View.VISIBLE);
-                mCheckLeft.setVisibility(View.VISIBLE);
+                mCheckRight.setVisibility(View.VISIBLE);
                 mMoveForward.setVisibility(View.VISIBLE);
                 mNothingHere.setVisibility(View.INVISIBLE);
 
@@ -79,6 +94,26 @@ public class CaveActivity extends AppCompatActivity {
             mMoveForward.setVisibility(View.INVISIBLE);
             mNothingHere.setVisibility(View.VISIBLE);
         }
+        if(mSegmentTracker == 2){
+                mCaveSegment = (TextView) findViewById(R.id.caveSegment);
+                mCheckLeft = (TextView) findViewById(R.id.checkLeft);
+                mCheckRight = (TextView) findViewById(R.id.checkRight);
+                mMoveForward = (TextView) findViewById(R.id.moveForward);
+                mNothingHere = (TextView) findViewById(R.id.nothingHere);
+                mWiredEel = (ImageView) findViewById(R.id.wiredEel);
+            mScaryEel = (TextView) findViewById(R.id.scaryEel);
+
+
+                mCaveSegment.setVisibility(View.INVISIBLE);
+                mCheckLeft.setVisibility(View.INVISIBLE);
+                mCheckRight.setVisibility(View.INVISIBLE);
+                mMoveForward.setVisibility(View.INVISIBLE);
+                mNothingHere.setVisibility(View.INVISIBLE);
+                mWiredEel.setVisibility(View.VISIBLE);
+                mScaryEel.setVisibility(View.VISIBLE);
+
+
+            }
         if(mSegmentTracker == 3){
             mCaveSegment = (TextView) findViewById(R.id.caveSegment);
             mCheckLeft = (TextView) findViewById(R.id.checkLeft);
---
+++ app/src/main/java/com/example/colleenminor/theadventure/ui/water/EelActivity.java
@@ -0,0 +1,86 @@
+package com.example.colleenminor.theadventure.ui.water;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.colleenminor.theadventure.R;
+import com.example.colleenminor.theadventure.models.Item;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EelActivity extends AppCompatActivity {
+    private SharedPreferences mPreferences;
+    private TextView mNoItems;
+    private Button mBackButton;
+    private ArrayList<Item> mItems;
+    private ListView lv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_eel);
+        mPreferences = getApplicationContext().getSharedPreferences("TheAdventure", Context.MODE_PRIVATE);
+        mNoItems = (TextView) findViewById(R.id.noItems);
+        mBackButton = (Button) findViewById(R.id.backButton);
+        //  getActionsFromIntent();
+        String username = mPreferences.getString("username", null);
+        mItems = (ArrayList) Item.all();
+        if (mItems.size() == 0){
+            mNoItems.setVisibility(TextView.VISIBLE);
+        }
+
+        mBackButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(EelActivity.this, MermaidActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        lv = (ListView) findViewById(R.id.listView);
+
+        List<String> itemStringList = new ArrayList<>();
+        for(int i = 0; i < mItems.size(); i++){
+            Item item = mItems.get(i);
+            itemStringList.add(item.getItem());
+        }
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_list_item_1,
+                itemStringList);
+        lv.setAdapter(arrayAdapter);
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1, int position, long arg3) {
+                String theItem = (String) arg0.getItemAtPosition(position);
+                if (!(theItem.equals("crab"))) {
+                    Toast toast = Toast.makeText(getApplicationContext(),
+                            "You can't kill an eel with the " + arg0.getItemAtPosition(position) + "!", Toast.LENGTH_SHORT);
+                    toast.show();
+                }
+                if (theItem.equals("crab")) {
+                    Toast toast = Toast.makeText(getApplicationContext(),
+                            "DIE, SEO!!!", Toast.LENGTH_LONG);
+                    toast.show();
+                    Item.delete("crab");
+                    Intent intent = new Intent(EelActivity.this, MermaidPalaceActivity.class);
+                    startActivity(intent);
+
+                }
+            }
+        });
+    }
+
+}
